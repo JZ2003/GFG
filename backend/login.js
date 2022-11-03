f9ce59e4b570f52e9d54d8f2748939f9150eec1a
haoyu li <haoyul1@vmware.com>
2022-11-02 20:59:01 -0700

    Basic implementation for login feature

---
+++ backend/login.js
@@ -0,0 +1,31 @@
+function handleSignupRequest(req, res) {
+  const { headers } = req;
+  const created = false;
+  // Todo: Post this body to the database;
+  // set flag iff post succeed
+  res.send(201, {"result": created});
+  return created;
+}
+
+function handleLoginRequest(req, res) {
+  const queryObject = url.URLSearchParams;
+  const username = queryObject.get(user);
+  const password = queryObject.get(pass);
+  const canLogin = validate(username, password);
+  res.send(200, {"result": canLogin});
+}
+
+function handleCancleRequest(req, res) {
+  const deleted = false;
+  //Todo: Delete account according to username.
+  //set 'deleted' true if delete succeed.
+  res.send(204, {"result": deleted});
+  return deleted;
+}
+
+function validate(username, password) {
+  //Todo: Search through database to see if the user have authorization
+  return false;
+}
+
+module.exports = { handleLoginRequest, handleSignupRequest, handleCancleRequest };
