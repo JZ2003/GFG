4faba6e715880027fef0b6d2e76d299f082e130b
yuesha-yc <kevinwangyichen@gmail.com>
2022-11-05 01:20:07 -0700

    Remove shebang code for now

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-cd backend
 # Start the database
 brew services start mongodb-community@6.0
 # Start the node server
--- stop-server.sh
+++ stop-server.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-cd backend
 # Start the database
 brew services stop mongodb-community@6.0
 # TODO: Stop the node server
