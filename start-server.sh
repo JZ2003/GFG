484c7a9475e4ba8ce0351fc61898099b4c4713f4
yuesha-yc <kevinwangyichen@gmail.com>
2022-11-04 23:39:56 -0700

    Update Readme and introduce server scripts

---
+++ start-server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd backend
+# Start the database
+brew services start mongodb-community@6.0
+# Start the server
+npm start
---
+++ stop-server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd backend
+# Start the database
+brew services stop mongodb-community@6.0
+# TODO: Stop the node server
