75df36e3a520bef7e93f394cbfc0624546bbeb11
yuesha-yc <kevinwangyichen@gmail.com>
2022-11-18 02:28:04 -0800

    Try sending POST to signup.

--- backend/expressapp.js
+++ backend/expressapp.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require('cors');
+const login = require('./login');
+const upload = require('./upload')
 
 const app = express();
 
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -13,18 +13,15 @@ const Signup = () => {
             body: JSON.stringify({
                 title: '',
                 body: '',
-                userId: Math.random().toString(36).slice(2),
-                user: user,
-                pass: pass
+                userId: Math.random().toString(36).slice(2)
             }),
             headers: {
-                'Content-type': 'application/json; charset=UTF-8'
+                'Content-type': 'application/json; charset=UTF-8',
+                'username': user,
+                'password': pass
             },
         })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-            })
+            .then((response) => console.log(response))
             .catch((err) => {
                 console.log(err.message);
             });
@@ -40,13 +37,16 @@ const Signup = () => {
     return (
         <div className="app">
             <div className="add-post-container">
-                <form onSubmit={handleSubmit}>
-                    <label for="user" > Username or Email: </label><br></br>
-                    <input type="text" className="form-control" value={user} onChange={(e) => setUser(e.target.value)} /><br></br>
-                    <label for="pass" > Password: </label><br></br>
-                    <input type="text" className="form-control" value={pass} onChange={(e) => setPass(e.target.value)} /><br></br>
-                    <button type="submit">Add Post</button>
-                </form>
+                <h1><center>Log In or Sign Up Here!</center></h1>
+                <center>
+                    <form onSubmit={handleSubmit}>
+                        <label for="user" > Username or Email: </label><br></br>
+                        <input type="text" className="form-control" value={user} onChange={(e) => setUser(e.target.value)} /><br></br>
+                        <label for="pass" > Password: </label><br></br>
+                        <input type="text" className="form-control" value={pass} onChange={(e) => setPass(e.target.value)} /><br></br>
+                        <button type="submit">Signup</button>
+                    </form>
+                </center>
             </div>
         </div>
     );
