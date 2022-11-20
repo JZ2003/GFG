f5231bd514f0d8e4ef39fdbba92c072f03e2f1c8
afangster <ahtfang@gmail.com>
2022-11-19 22:09:25 -0800

    figuring out how to display a users mods

--- frontend/src/pages/mymods.js
+++ frontend/src/pages/mymods.js
@@ -6,18 +6,39 @@ class My_Mods extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            modName: props.modName, 
-            author: 'sunny',
-            desc: '',
-            dateCreated: '2002/11/3',
-            dateModified: '2022/11/18',
-            url: 'http',
-            gameName: 'Persona 5',
-            tag: 'functional',
-            views: '50',
-            icon: 'https://www.gannett-cdn.com/presto/2020/04/02/PREN/1a3f1556-6174-4ed1-8881-4bbb8ab12f49-Cast.jpg'
+            user: localStorage.getItem('user'),
+            modName: "",
+            mods: []
         };
+        this.getInfo(this.state.user);
     }
+
+    async getInfo (user) {
+        if(user == null){
+            console.log("not logged in");
+        }
+        await fetch('http://localhost:3030/filterMod', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                "filter": "{author: " + user +"}"
+            },
+        })
+        .then((response) => {
+            console.log(response);
+            if(response.status == 200){
+                console.log(response.parse());
+                this.state.mods = response.parse();
+                console.log("fetched");
+            }
+            else{
+                console.log('did not succeed');
+            }
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+    };
 
     render(){
         return (
@@ -27,28 +48,20 @@ class My_Mods extends React.Component{
                     marginTop: '20px',
                 }}>
                 <h1>Mod Name: {this.state.modName}</h1>
-                <img className='modIcon' src={this.state.url} alt='mod icon'></img>
-                <h2>Author: {this.state.author}</h2>
-                <h3>Desc: {this.state.desc}</h3>
-                <h4>Date Created: {this.state.dateCreated}</h4>
-                <h5>Date Modified: {this.state.dateModified}</h5>
-                <h6>Download URL: {this.state.url}</h6>
-                <h7>Game: {this.state.gameName}     </h7>
-                <h8>tag: {this.state.tag}      </h8>
-                <h9>views: {this.state.views}     </h9>
+                
             </div>
             
         );
 
-        return (
-            <div style = {{
-                    marginLeft: '10%',
-                    marginTop: '20px',
-                    width: '50%'
-                }}>
-                <h1>Your Mods: </h1>
-            </div>
-        );
+        // return (
+        //     <div style = {{
+        //             marginLeft: '10%',
+        //             marginTop: '20px',
+        //             width: '50%'
+        //         }}>
+        //         <h1>Your Mods: </h1>
+        //     </div>
+        // );
     };
 }
 
