faf3a5649bb8ff9f2bab7b7cd0056cb69a0f35ef
afangster <ahtfang@gmail.com>
2022-11-21 01:15:04 -0800

    working on clickable itemizing

--- frontend/src/pages/mymods.js
+++ frontend/src/pages/mymods.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import {useLocation} from 'react-router-dom';
+import './mymods.css'
 
 class My_Mods extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             user: localStorage.getItem('user'),
+            signedIn: false,
+            modName: "",
+            gameName: "",
+            Desc: "",
+            mods: []
         };
         this.getInfo(this.state.user);
     }
 
+    // componentDidMount(){
+    //     this.getInfo = this.getInfo(this.state.user);
+    //   }
+
     async getInfo (user) {
+        console.log("user is: " + user);
         if(user == null){
             console.log("not logged in");
+        }
+        else{
+            this.state.signedIn = true;
+            console.log(this.state.signedIn);
         }
         await fetch('http://localhost:3030/filterMod', {
             method: 'GET',
@@ -25,14 +40,20 @@ class My_Mods extends React.Component{
         .then((response) => {
             if(response.status === 200){
                 this.state.mods = response.json().then((data) => {
-                    console.log(data);
+                    console.log(data.length);
+                    console.log(data[0].modName);
+                    // this.state.modName = data[0].modName;
+                    // this.state.mods = data;
                     this.setState({modName: data[0].modName});
                     this.setState({mods: data});
+                    this.setState({gameName: data[0].gameName});
+                    this.setState({Desc: data[0].desc});
+                    console.log(this.state.modName);
                 });
                 console.log("fetched");
             }
             else{
-                console.log('did not succeed');
+                console.log('no mods associated with user');
             }
         })
         .catch((err) => {
@@ -47,8 +68,21 @@ class My_Mods extends React.Component{
                     marginRight: '10%',
                     marginTop: '20px',
                 }}>
-                <h1>Yours Mods: </h1><br/>
-                <h2>Mod Name: {this.state.modName}</h2>
+                {!this.state.signedIn && <center><h1>Please sign in.</h1></center>}
+                {this.state.signedIn && <h1>Your uploaded mods </h1>}<br/>
+                <a href={"http://localhost:3000/" + this.state.modName} class="card">
+                    <article class="text">
+                        {this.state.signedIn && 
+                            <p>
+                                Mod Name: {this.state.modName}<br/> 
+                                Game Name: {this.state.gameName}<br/>
+                                Description: {this.state.Desc}
+                            </p>
+                        }
+                    </article>
+                </a>
+                {/* {this.state.signedIn && <p>Game Name: {this.state.gameName}</p>}<br/>
+                {this.state.signedIn && <p>Description: {this.state.Desc}</p>}<br/> */}
                 
             </div>
             
