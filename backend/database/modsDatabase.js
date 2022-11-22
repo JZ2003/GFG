264aa20bf129c9ed3260e2bb3a2028d243a22fd7
jz_2003 <zhaojianxing2003@gmail.com>
2022-11-21 22:35:44 -0800

    Implement handleUpdateRequst. It's used for updating basic mod information

--- backend/database/modsDatabase.js
+++ backend/database/modsDatabase.js
@@ -105,7 +105,6 @@ async function insertDummyMods(numMods) {
     if (numMods <= 0) {
         return null;
     }
-    //allUnique是否需要let？ --ZJX
     allUnique = true;
     for (let i = 0; i < numMods; i++) {
         const mod = new Mod(
