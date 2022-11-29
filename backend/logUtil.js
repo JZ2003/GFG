7e6a85e80305a09e31a1fc957200f76a4ce4ede8
yuesha-yc <kevinwangyichen@gmail.com>
2022-11-29 02:51:13 -0800

    Need to npm install in backend!

--- backend/logUtil.js
+++ backend/logUtil.js
@@ -1,3 +1,16 @@
+/**
+ * logUtil.js
+ * 
+ * To use this logger, you need to import it and call getLogger with a label.
+ * Example:
+ * const logUtil = require('./logUtil');
+ * const logger = logUtil.getLogger('myLabel');
+ * logger.info('Hello world');
+ * logger.error('Error');
+ * logger.warn('Warning');
+ * logger.debug('Debug');
+ */
+
 const { createLogger, format, transports } = require("winston");
 const { combine, label, printf} = format;
 
